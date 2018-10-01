e2cf3b7cf1f659d668f6db0dcc80f64a53c296b5
taku paku <takupaku@yahoo.com>
2018-10-01 18:55:13 +0600

    delete

--- app/src/main/java/com/example/student/studentdatabasewithfirebase/activity/ListViewActivity.java
+++ app/src/main/java/com/example/student/studentdatabasewithfirebase/activity/ListViewActivity.java
@@ -1,12 +1,17 @@
 package com.example.student.studentdatabasewithfirebase.activity;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.student.studentdatabasewithfirebase.R;
+import com.example.student.studentdatabasewithfirebase.adapter.StudentAdapter;
 import com.example.student.studentdatabasewithfirebase.model.Student;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -44,9 +49,13 @@ public class ListViewActivity extends AppCompatActivity {
                 }
 
 
-                Toast.makeText(ListViewActivity.this, ""+studentList.get(0).getName(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(ListViewActivity.this, ""+studentList.get(0).getName(), Toast.LENGTH_SHORT).show();
 
+                StudentAdapter adapter = new StudentAdapter(ListViewActivity.this,studentList);
+                listView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
             }
+
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
@@ -62,5 +71,36 @@ public class ListViewActivity extends AppCompatActivity {
 
     private void initView() {
         listView = findViewById(R.id.ivLd);
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                showDialoge(i);
+
+                return false;
+            }
+        });
+    }
+
+    private void showDialoge(final int pos) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("WILL YOU EDIT THIS?");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //DELETE STUDENT
+                reference.child(studentList.get(pos).getUser_id()).removeValue();
+            }
+        });
+
+        builder.setNegativeButton("Edit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //this will edit
+            }
+        });
+        builder.setNeutralButton("cancel",null);
+        builder.show();
     }
 }
