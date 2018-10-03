712a169fcb1477672e7fe12accb93e550b88bbad
taku paku <takupaku@yahoo.com>
2018-10-03 17:13:54 +0600

    update

--- app/src/main/java/com/example/student/studentdatabasewithfirebase/activity/ListViewActivity.java
+++ app/src/main/java/com/example/student/studentdatabasewithfirebase/activity/ListViewActivity.java
@@ -1,18 +1,24 @@
 package com.example.student.studentdatabasewithfirebase.activity;
 
 import android.content.DialogInterface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.student.studentdatabasewithfirebase.R;
 import com.example.student.studentdatabasewithfirebase.adapter.StudentAdapter;
 import com.example.student.studentdatabasewithfirebase.model.Student;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,6 +34,7 @@ public class ListViewActivity extends AppCompatActivity {
     private DatabaseReference reference;
     private List<Student>  studentList = new ArrayList<>();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +49,8 @@ public class ListViewActivity extends AppCompatActivity {
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+
+                studentList.clear();
                 Iterable<DataSnapshot> allStudents = dataSnapshot.getChildren();
                 for(DataSnapshot student : allStudents){
                    Student std1 = student.getValue(Student.class);
@@ -97,10 +106,64 @@ public class ListViewActivity extends AppCompatActivity {
         builder.setNegativeButton("Edit", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
+
+                updateTheStudent(pos);
                 //this will edit
             }
         });
         builder.setNeutralButton("cancel",null);
         builder.show();
+    }
+
+    //update here
+    private void updateTheStudent(int pos) {
+        String id = studentList.get(pos).getId();
+        String name = studentList.get(pos).getName();
+        String dept = studentList.get(pos).getDept();
+        final String user_node = studentList.get(pos).getUser_id();
+
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.update_layout,null);
+
+        final EditText etId = view.findViewById(R.id.updateId);
+        final EditText etName = view.findViewById(R.id.update_name);
+        final Spinner spDept = view.findViewById(R.id.update_dept);
+        Button updateBtn = view.findViewById(R.id.updateButton);
+
+        etId.setText(id);
+        etName.setText(name);
+        String[] deptList = getResources().getStringArray(R.array.dept);
+        for(int i = 0; i<deptList.length; i++){
+            if(deptList[i].equals(dept)){
+                spDept.setSelection(i);
+            }
+        }
+
+        builder.setView(view);
+        builder.setCancelable(false);
+        final AlertDialog dialog = builder.show();
+
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newId = etId.getText().toString().trim();
+                String newName = etName.getText().toString().trim();
+                String newDept = spDept.getSelectedItem().toString();
+                reference.child(user_node).setValue(new Student());
+
+                reference.child(user_node).setValue(new Student(newId,newName,newDept,user_node)).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(ListViewActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(ListViewActivity.this, "not Updated", Toast.LENGTH_SHORT).show();
+                        }
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
     }
 }
